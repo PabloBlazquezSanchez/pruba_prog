c84510121a7a539917ffbd1e90d277563b7a0506
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2021-12-17 10:25:11 +0100

    Implementando suma

--- src/prueba_github/Principal.java
+++ src/prueba_github/Principal.java
@@ -5,7 +5,12 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		System.out.println(suma(4, 5));
 
+	}
+	public static int suma(int a, int b) {
+		return a+b;
 	}
 
 }
