e9ee1f612a031330b6e9480f1b7c89976d820f07
jesusfdez1 <96061251+jesusfdez1@users.noreply.github.com>
2022-03-10 18:15:23 +0100

    Añadido clase Promoción

---
+++ src/prueba_github/Promocion.java
@@ -0,0 +1,47 @@
+package prueba_github;
+
+public class Promocion {
+	private long idPromocion;
+	private boolean cartel;
+	private boolean redesSociales;
+	private Contenido contenido;
+
+	public Promocion(long idPromocion, Contenido contenido, boolean cartel, boolean redesScoiales) {
+		this.idPromocion = idPromocion;
+		this.cartel = cartel;
+		this.redesSociales = redesSociales;
+		this.contenido = contenido;
+	}
+
+	public long getIdPromocion() {
+		return idPromocion;
+	}
+
+	public void setIdPromocion(long idPromocion) {
+		this.idPromocion = idPromocion;
+	}
+
+	public boolean isCartel() {
+		return cartel;
+	}
+
+	public void setCartel(boolean cartel) {
+		this.cartel = cartel;
+	}
+
+	public boolean isRedesSociales() {
+		return redesSociales;
+	}
+
+	public void setRedesSociales(boolean redesSociales) {
+		this.redesSociales = redesSociales;
+	}
+
+	public Contenido getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(Contenido contenido) {
+		this.contenido = contenido;
+	}
+}
