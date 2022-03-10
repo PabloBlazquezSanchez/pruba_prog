cf034bccc1539ca788a4ab05064f7c34960747c8
jesusfdez1 <96061251+jesusfdez1@users.noreply.github.com>
2022-03-10 14:19:51 +0100

    Update pelicula.java

--- src/prueba_github/Pelicula.java
+++ src/prueba_github/Pelicula.java
@@ -1,11 +1,34 @@
 package prueba_github;
 
-public class Pelicula {
-	private String Origen;
-	private String Porductora;
+public class Pelicula extends Contenido {
+	private String origen;
+	private String productora;
 
-	public Pelicula(String titulo, int fechaEstreno, int duracion, String Sinopsis, boolean tendencias, int temporadas,
-			int capitulos) {
-
+	public Pelicula(String titulo, int fechaestreno, int duracion, String sinopsis, boolean tendencias, String origen,
+			String productora) {
+		super(titulo, fechaestreno, duracion, sinopsis, tendencias);
+		this.origen = origen;
+		this.productora = productora;
 	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getProductora() {
+		return productora;
+	}
+
+	public void setProductora(String productora) {
+		this.productora = productora;
+	}
+
+	public double calcularPrecioPromocion() {
+		return 0;
+	}
+
 }
