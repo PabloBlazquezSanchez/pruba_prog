efd1ef748b8c668c4d846fccd873f49286102f10
jesusfdez1 <96061251+jesusfdez1@users.noreply.github.com>
2022-03-10 13:57:04 +0100

    prueba1

---
+++ src/prueba_github/Pelicula.java
@@ -0,0 +1,11 @@
+package prueba_github;
+
+public class Pelicula {
+	private String Origen;
+	private String Porductora;
+
+	public Pelicula(String titulo, int fechaEstreno, int duracion, String Sinopsis, boolean tendencias, int temporadas,
+			int capitulos) {
+
+	}
+}
