6d05d794c624cbd502a7d705d3ceec86b50f9422
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-03-10 14:09:49 +0100

    yyy

---
+++ src/prueba_github/Contenido.java
@@ -0,0 +1,58 @@
+package prueba_github;
+
+public abstract class Contenido {
+	private String titulo;
+	private int fechaestreno, duracion;
+	private String sinopsis;
+	private boolean tendencias;
+	
+	public Contenido (String titulo, int fechaestreno, int duracion, String sinopsis, boolean tendencias) {
+		this.titulo=titulo;
+		this.fechaestreno=fechaestreno;
+		this.duracion=duracion;
+		this.sinopsis=sinopsis;
+		this.tendencias=tendencias;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getFechaestreno() {
+		return fechaestreno;
+	}
+
+	public void setFechaestreno(int fechaestreno) {
+		this.fechaestreno = fechaestreno;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public String getSinopsis() {
+		return sinopsis;
+	}
+
+	public void setSinopsis(String sinopsis) {
+		this.sinopsis = sinopsis;
+	}
+
+	public boolean isTendencias() {
+		return tendencias;
+	}
+
+	public void setTendencias(boolean tendencias) {
+		this.tendencias = tendencias;
+	}
+	
+	public abstract double calcularPrecioPromocion();
+}
