fa7951cddd790cf749e952c502b296693b5e4ef0
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-03-10 13:41:28 +0100

    cambio clase princippal

--- src/prueba_github/Principal.java
+++ src/prueba_github/Principal.java
@@ -2,15 +2,8 @@ package prueba_github;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+	public Principal() {
 		
-		System.out.println(suma(4, 5));
-
-	}
-	public static int suma(int a, int b) {
-		return a+b;
 	}
 
 }
