abcad127356de580e5d812f20983d626413a0517
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-03-10 19:09:28 +0100

    indentaciones empresamarketing

--- src/prueba_github/EmpresaMarketing.java
+++ src/prueba_github/EmpresaMarketing.java
@@ -1,39 +1,39 @@
 package prueba_github;
 
 public class EmpresaMarketing {
-private String nombre;
-private long telefono;
-private double precioCampañaPelicula;
+	private String nombre;
+	private long telefono;
+	private double precioCampañaPelicula;
 
-EmpresaMarketing(String nombre, long telefono, double precioCamapañaPelicula){
-	this.nombre=nombre;
-	this.telefono=telefono;
-	this.precioCampañaPelicula=precioCampañaPelicula;
-}
+	EmpresaMarketing(String nombre, long telefono, double precioCamapañaPelicula){
+		this.nombre=nombre;
+		this.telefono=telefono;
+		this.precioCampañaPelicula=precioCampañaPelicula;
+	}
 
-public String getNombre() {
-	return nombre;
-}
+	public String getNombre() {
+		return nombre;
+	}
 
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-public long getTelefono() {
-	return telefono;
-}
+	public long getTelefono() {
+		return telefono;
+	}
 
-public void setTelefono(long telefono) {
-	this.telefono = telefono;
-}
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
 
-public double getPrecioCampañaPelicula() {
-	return precioCampañaPelicula;
-}
+	public double getPrecioCampañaPelicula() {
+		return precioCampañaPelicula;
+	}
 
-public void setPrecioCampañaPelicula(double precioCampañaPelicula) {
-	this.precioCampañaPelicula = precioCampañaPelicula;
-}
+	public void setPrecioCampañaPelicula(double precioCampañaPelicula) {
+		this.precioCampañaPelicula = precioCampañaPelicula;
+	}
 
-k jlefnke//PREGUNTAR CALCULARPRESUPUESTO 
+//PREGUNTAR CALCULARPRESUPUESTO 
 }
