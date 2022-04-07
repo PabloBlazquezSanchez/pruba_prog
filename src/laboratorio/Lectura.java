c62b9172c1ef4ceaffac57a8bbc3a93098d274e9
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-04-07 17:45:13 +0200

    update

--- src/laboratorio/Lectura.java
+++ src/laboratorio/Lectura.java
@@ -3,16 +3,73 @@ package laboratorio;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Lectura {
 
+	static Netflix n = new Netflix (); 
+
 	public static void main(String[] args) throws IOException {
 		leerFichero2("Contenido.csv");
-		mostrarContenido();
+		mostrarMenu();
+		
 	}
 	
+	public static void mostrarMenu() {
+		int opcion=0;
+		// TODO Auto-generated method stub
+		do {
+			System.out.println("Menú: \n"+"1. Mostrar toda la información del contenido de la plataforma.\n"
+					+"2. Realizar una promoción de una película o una serie.\n"
+					+"3. Mostrar todas las promociones realizadas.\n"
+					+"4. Mostrar coste de todas las promociones realizadas.\n"
+					+"5. Calcular coste de promoción de una película o serie en concreto."
+					+"6. Calcular el coste de una campaña masiva de marketing.\n"
+					+"7. Calcular la subvención que se obtiene a partir de las películas o series.\n"
+					+"Escriba el número de la opción que desea realizar: ");
+			opcion = filtrarEscritura();
+			switch(opcion) {
+			case 1:
+				mostrarContenido();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				System.out.println("Programa finalizado.");
+				break;
+			default:
+				System.out.println("Error. Vuelva a escribir la opción otra vez.\n");
+				break;
+			}
+		}while(opcion !=8);
+		
+	}
+
+	public static int filtrarEscritura() {
+		// TODO Implementar despues un error de escritura
+		int numero = 0;
+		Scanner lectura = new Scanner(System.in);
+		try {
+			numero = lectura.nextInt();
+		}catch(InputMismatchException ime) {
+			System.out.println("Sólo puede escribir números. Inténtalo de nuevo: ");
+			numero = filtrarEscritura();
+		}
+		return numero;
+	}
+
 	private static void mostrarContenido() {
-		Netflix n = new Netflix();
 		n.mostrarContenido();
 		
 	}
@@ -23,7 +80,6 @@ public class Lectura {
 		boolean tendencia;
 		Contenido contenido;
 		String line;
-		Netflix n = new Netflix (); 
 		try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
 			while ((line = br.readLine()) != null) {
 				String[] split = line.split(";");
