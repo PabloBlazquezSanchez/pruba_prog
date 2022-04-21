8341b4b3941599f978744ae3c9a45ef7ad6130da
jesusfdez1 <96061251+jesusfdez1@users.noreply.github.com>
2022-04-21 10:26:26 +0200

    actual

---
+++ src/exceptions/ContenidoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ContenidoNoEncontradoException extends Exception {
+	public ContenidoNoEncontradoException(String msg) {
+		super(msg);
+	}
+}
--- src/laboratorio/Netflix.java
+++ src/laboratorio/Netflix.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import exceptions.ContenidoNoEncontradoException;
 
 public class Netflix {
 	private Scanner leer = new Scanner(System.in);
